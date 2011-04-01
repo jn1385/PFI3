386482e32b1256c8d5d804dcbee203c0b1e90463
johannesnilsson <jn1385@gmail.com>
2011-04-01 15:31:16 +0200

    Signed-off-by: johannesnilsson <jn1385@gmail.com>

---
+++ test4/src/se/mah/k3/nilsson/johannes/test4/test4.java
@@ -0,0 +1,13 @@
+package se.mah.k3.nilsson.johannes.test4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class test4 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
