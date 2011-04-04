dcaafc08f517ff0038209d8665dbfadfbff170f8
johannesnilsson <jn1385@gmail.com>
2011-04-04 10:45:07 +0200

    Signed-off-by: johannesnilsson <jn1385@gmail.com>

---
+++ pfi3/test5/src/se/mah/k3/nilsson/johannes/test5/test5.java
@@ -0,0 +1,13 @@
+package se.mah.k3.nilsson.johannes.test5;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class test5 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
